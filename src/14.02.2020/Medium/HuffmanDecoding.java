adaa00994c97cdb1408e379c93e568de778c32df
LalithaDevi-source <you@example.com>
2020-04-14 19:03:34 +0530

    committing the medium problems

---
+++ src/14.02.2020/Medium/HuffmanDecoding.java
@@ -0,0 +1,67 @@
+abstract class Node implements Comparable<Node> {
+    
+  	public  int frequency; // the frequency of this tree
+    public  char data;
+    public  Node left, right; 
+    public Node(int freq) { 
+    	frequency = freq;
+   	}
+ 
+    // compares on the frequency
+    public int compareTo(Node tree) {
+        return frequency - tree.frequency;
+    }
+  
+}
+ 
+class HuffmanLeaf extends Node {
+    
+    public HuffmanLeaf(int freq, char val) {
+        super(freq);
+        data = val;
+    }
+  
+}
+ 
+class HuffmanNode extends Node {
+    
+    public HuffmanNode(Node l, Node r) {
+        super(l.frequency + r.frequency);
+        left = l;
+        right = r;
+    }
+
+}
+
+
+class Decoding {
+
+/*  
+	class Node
+		public  int frequency; // the frequency of this tree
+    	public  char data;
+    	public  Node left, right;
+    
+*/ 
+
+    void decode(String s, Node root) {
+
+        Node node = root;
+        for (char ch : s.toCharArray()) {
+            if (ch == '0') {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+
+            if (node.data != 0) {
+                System.out.print(node.data);
+
+                node = root;
+            }
+        }
+
+
+    }
+}
+
---
+++ src/14.02.2020/Medium/IsThisABinaryTree.java
@@ -0,0 +1,22 @@
+class Node 
+{
+        int data;
+        Node left;
+        Node right;
+}
+ 
+public class IsThisABinaryTree {
+	
+	boolean checkBST(Node root) {
+		return check(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+	}
+	boolean check(Node n, int min, int max){
+		if(n==null)
+			return true;
+		if(n.data <= min || n.data >= max)
+			return false;
+		return check(n.left, min, n.data) 
+			&& check(n.right, n.data, max);
+	}
+
+}
